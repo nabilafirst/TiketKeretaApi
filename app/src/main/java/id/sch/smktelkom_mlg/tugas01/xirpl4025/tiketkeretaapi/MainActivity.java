822fff7ce9eb3a4d66ce7e8bc3ed2aedcf747ab7
Nabilafirst <nabilafirsta@gmail.com>
2016-09-09 22:09:23 +0700

    fourth commit : button sudah berfungsi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4025/tiketkeretaapi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4025/tiketkeretaapi/MainActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.tugas01.xirpl4025.tiketkeretaapi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -43,6 +44,112 @@ public class MainActivity extends AppCompatActivity {
         rbP = (RadioButton) findViewById(R.id.radioButtonP);
 
         bOk = (Button) findViewById(R.id.buttonOK);
+        tvPesan = (TextView) findViewById(R.id.textViewPesanan);
 
+        bOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doProcess();
+            }
+        });
+
+    }
+
+    private void doProcess() {
+
+        if (isValid()) {
+
+            String jk;
+            String kelas;
+            String nama = etNama.getText().toString();
+            String alamat = etAlamat.getText().toString();
+            String asal = etAsal.getText().toString();
+            String tujuan = etTujuan.getText().toString();
+            String tanggal = etTanggal.getText().toString();
+            String jam = etJam.getText().toString();
+            int tahun = Integer.parseInt(etTahun.getText().toString());
+            int usia = 2016 - tahun;
+
+            if (rbL.isChecked()) {
+                jk = rbL.getText().toString();
+            } else {
+                jk = rbP.getText().toString();
+            }
+
+            if (cbBisnis.isChecked()) {
+                kelas = cbBisnis.getText().toString();
+            } else if (cbEksekutif.isChecked()) {
+                kelas = cbEksekutif.getText().toString();
+            } else {
+                kelas = cbEkonomi.getText().toString();
+            }
+
+            tvPesan.setText("Terimakasih " + nama + ", pesanan Anda telah berhasil diproses. Berikut data diri Anda : \n"
+                    + "Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + usia + " tahun \nJenis Kelamin : " + jk +
+                    "\nKereta Api Kelas " + kelas + " dengan rute " + asal + " ke " + tujuan + " pada tanggal " + tanggal + " pukul " + jam + " WIB");
+
+        }
+
+    }
+
+    public boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        String tahun = etTahun.getText().toString();
+        String alamat = etAlamat.getText().toString();
+        String asal = etAsal.getText().toString();
+        String tujuan = etTujuan.getText().toString();
+        String tanggal = etTanggal.getText().toString();
+        String jam = etJam.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama Belum Diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+
+        if (tahun.isEmpty()) {
+            etTahun.setError("Tahun Kelahiran belum diisi");
+            valid = false;
+        } else if (tahun.length() != 4) {
+            etTahun.setError("Format Tahun Salah");
+        } else {
+            etTahun.setError(null);
+        }
+
+        if (alamat.isEmpty()) {
+            etAlamat.setError("Alamat belum diisi");
+            valid = false;
+        } else {
+            etAlamat.setError(null);
+        }
+
+        if (asal.isEmpty()) {
+            etAsal.setError("Kota Asal belum diisi");
+            valid = false;
+        } else {
+            etAsal.setError(null);
+        }
+
+        if (tujuan.isEmpty()) {
+            etTujuan.setError("Kota Tujuan belum diisi");
+            valid = false;
+        } else {
+            etTanggal.setError(null);
+        }
+
+        if (jam.isEmpty()) {
+            etJam.setError("Jam Keberangkatan belum diisi");
+            valid = false;
+        } else {
+            etJam.setError(null);
+        }
+
+        return valid;
     }
 }
